98fb86a2bf323e7d1d74fc1b696f85f14f77890e
Pekka Alaluukas <alaluuk@oamk.fi>
2021-03-23 10:13:43 +0200

    allpersons started

--- qt_frontend/mainwindow.cpp
+++ qt_frontend/mainwindow.cpp
@@ -13,3 +13,23 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_btnGetAllPersons_clicked()
+{
+    QString site_url="http://localhost:3000/example/allpersons";
+    QString credentials="automat123:pass123";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QByteArray data = credentials.toLocal8Bit().toBase64();
+    QString headerData = "Basic " + data;
+    request.setRawHeader( "Authorization", headerData.toLocal8Bit() );
+    allPersonsManager = new QNetworkAccessManager(this);
+    connect(allPersonsManager, SIGNAL(finished (QNetworkReply*)),
+    this, SLOT(getBookSlot(QNetworkReply*)));
+    allPersonsReply = allPersonsManager->get(request);
+}
+
+void MainWindow::allPersonsSlot(QNetworkReply *reply)
+{
+
+}
--- qt_frontend/mainwindow.h
+++ qt_frontend/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btnGetAllPersons_clicked();
+    void allPersonsSlot(QNetworkReply *reply);
+
 private:
     Ui::MainWindow *ui;
+    QNetworkAccessManager *allPersonsManager;
+    QNetworkReply *allPersonsReply;
 };
 #endif // MAINWINDOW_H
