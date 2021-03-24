1ee645c2715db9298d1fb21421cb57add501a818
Pekka Alaluukas <alaluuk@oamk.fi>
2021-03-24 09:18:20 +0200

    get examples ready

--- qt_frontend/mainwindow.h
+++ qt_frontend/mainwindow.h
@@ -21,10 +21,24 @@ public:
 private slots:
     void on_btnGetAllPersons_clicked();
     void allPersonsSlot(QNetworkReply *reply);
+    void onePersonSlot(QNetworkReply *reply);
+    void fullnameSlot(QNetworkReply *reply);
+
+    void on_btnGetOnePerson_clicked();
+
+    void on_btnGetFullname_clicked();
 
 private:
     Ui::MainWindow *ui;
     QNetworkAccessManager *allPersonsManager;
     QNetworkReply *allPersonsReply;
+
+    QNetworkAccessManager *onePersonManager;
+    QNetworkReply *onePersonReply;
+
+
+    QNetworkAccessManager *fullnameManager;
+    QNetworkReply *fullnameReply;
+
 };
 #endif // MAINWINDOW_H
